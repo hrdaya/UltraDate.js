d951009c8b7c0dd41914dceed461e7d909ae7729
hrdaya <info@mar-cielo.com>
2015-03-02 10:21:38 +0900

    リファクタリング

--- dist/UltraDate.ja.js
+++ dist/UltraDate.ja.js
@@ -15,16 +15,16 @@
      * 使用するので片方だけの追加にならないように注意が必要
      * ロケールが設定されていない場合は「def」が使用される
      */
-    "use strict";
+    'use strict';
     /**
      * 定数
      */
     var consts = {
         defaultFormat: UltraDate.getDefaultFormat(), // デフォルトのフォーマット
-        strLocale: "ja", // デフォルトに設定するロケール
-        strFurikae: "振替休日", // 振替休日用の文字列
+        strLocale: 'ja', // デフォルトに設定するロケール
+        strFurikae: '振替休日', // 振替休日用の文字列
         dateFurikae: new Date(1973, 3, 12), // 振替休日の施行日
-        strKokumin: "国民の休日", // 国民の休日用の文字列
+        strKokumin: '国民の休日', // 国民の休日用の文字列
         dateKokumin: new Date(1985, 11, 27), // 国民の休日の施行日
         dateHeisei: new Date(1989, 0, 8), // 平成の始まり
         dateShowa: new Date(1926, 11, 25), // 昭和の始まり
@@ -39,49 +39,49 @@
     // 日付フォーマットのオプション追加
     UltraDate.setFormatOption(consts.strLocale,
             {
-                longDay: ["日曜日", "月曜日", "火曜日", "水曜日",
-                    "木曜日", "金曜日", "土曜日"],
-                shortDay: ["日", "月", "火", "水", "木", "金", "土"],
-                longMonth: ["睦月", "如月", "弥生", "卯月", "皐月", "水無月",
-                    "文月", "葉月", "長月", "神無月", "霜月", "師走"],
-                shortMonth: ["睦月", "如月", "弥生", "卯月", "皐月", "水無月",
-                    "文月", "葉月", "長月", "神無月", "霜月", "師走"],
-                smallNoon: ["AM", "PM"],
-                largeNoon: ["午前", "午後"],
+                longDay: ['日曜日', '月曜日', '火曜日', '水曜日',
+                    '木曜日', '金曜日', '土曜日'],
+                shortDay: ['日', '月', '火', '水', '木', '金', '土'],
+                longMonth: ['睦月', '如月', '弥生', '卯月', '皐月', '水無月',
+                    '文月', '葉月', '長月', '神無月', '霜月', '師走'],
+                shortMonth: ['睦月', '如月', '弥生', '卯月', '皐月', '水無月',
+                    '文月', '葉月', '長月', '神無月', '霜月', '師走'],
+                smallNoon: ['AM', 'PM'],
+                largeNoon: ['午前', '午後'],
                 era: function (date) {
                     var year = date.getFullYear();
                     var ret = {
-                        longName: "西暦",
-                        shortName: "西",
-                        alphaName: "AD",
+                        longName: '西暦',
+                        shortName: '西',
+                        alphaName: 'AD',
                         year: year
                     };
                     if (year > consts.yearHeisei) {
                         ret = {
-                            longName: "平成",
-                            shortName: "平",
-                            alphaName: "H",
+                            longName: '平成',
+                            shortName: '平',
+                            alphaName: 'H',
                             year: year - consts.yearHeisei
                         };
                     } else if (year > consts.yearShowa) {
                         ret = {
-                            longName: "昭和",
-                            shortName: "昭",
-                            alphaName: "S",
+                            longName: '昭和',
+                            shortName: '昭',
+                            alphaName: 'S',
                             year: year - consts.yearShowa
                         };
                     } else if (year > consts.yearTaisho) {
                         ret = {
-                            longName: "大正",
-                            shortName: "大",
-                            alphaName: "T",
+                            longName: '大正',
+                            shortName: '大',
+                            alphaName: 'T',
                             year: year - consts.yearTaisho
                         };
                     } else if (year > consts.yearMeiji) {
                         ret = {
-                            longName: "明治",
-                            shortName: "明",
-                            alphaName: "M",
+                            longName: '明治',
+                            shortName: '明',
+                            alphaName: 'M',
                             year: year - consts.yearMeiji
                         };
                     }
@@ -90,37 +90,37 @@
                 eraStrict: function (date) {
                     if (date >= consts.dateHeisei) {
                         return {
-                            longName: "平成",
-                            shortName: "平",
-                            alphaName: "H",
+                            longName: '平成',
+                            shortName: '平',
+                            alphaName: 'H',
                             year: date.getFullYear() - consts.yearHeisei
                         };
                     } else if (date >= consts.dateShowa) {
                         return {
-                            longName: "昭和",
-                            shortName: "昭",
-                            alphaName: "S",
+                            longName: '昭和',
+                            shortName: '昭',
+                            alphaName: 'S',
                             year: date.getFullYear() - consts.yearShowa
                         };
                     } else if (date >= consts.dateTaisho) {
                         return {
-                            longName: "大正",
-                            shortName: "大",
-                            alphaName: "T",
+                            longName: '大正',
+                            shortName: '大',
+                            alphaName: 'T',
                             year: date.getFullYear() - consts.yearTaisho
                         };
                     } else if (date >= consts.dateMeiji) {
                         return {
-                            longName: "明治",
-                            shortName: "明",
-                            alphaName: "M",
+                            longName: '明治',
+                            shortName: '明',
+                            alphaName: 'M',
                             year: date.getFullYear() - consts.yearMeiji
                         };
                     } else {
                         return {
-                            longName: "西暦",
-                            shortName: "西",
-                            alphaName: "AD",
+                            longName: '西暦',
+                            shortName: '西',
+                            alphaName: 'AD',
                             year: date.getFullYear()
                         };
                     }
@@ -181,7 +181,7 @@
                     // キーの順番に振替休日と国民の休日を確認していく
                     for (i = 0, len = keys.length; i < len; i++) {
                         // 該当する日付をパースして、ミリ秒の時間を取得
-                        var parse = UltraDate.parse(keys[i] + " 00:00:00");
+                        var parse = UltraDate.parse(keys[i] + ' 00:00:00');
 
                         // 日付をセット
                         date.setTime(parse);
@@ -212,21 +212,21 @@
                     // とりあえずその月の1日にセット
                     date.setFullYear(year, month, 1);
                     switch (toClassString.call(dateVal)) {
-                        case "[object Number]":
+                        case '[object Number]':
                             date.setDate(dateVal);
                             break;
-                        case "[object Array]":
+                        case '[object Array]':
                             date.setDayCountsInMonth(dateVal[0], dateVal[1]);
                             break;
-                        case "[object String]":
-                            if (toClassString.call(this[dateVal]) === "[object Function]") {
+                        case '[object String]':
+                            if (toClassString.call(this[dateVal]) === '[object Function]') {
                                 date.setDate(this[dateVal](year));
                             } else {
-                                throw new Error("指定の関数が存在しません");
+                                throw new Error('指定の関数が存在しません');
                             }
                             break;
                         default:
-                            throw new Error("引数のデータ型がおかしいです");
+                            throw new Error('引数のデータ型がおかしいです');
                     }
                     // キャッシュにデータを挿入する
                     this.cache[year][date.format(consts.defaultFormat)] = name;
@@ -291,56 +291,56 @@
                  *
                  */
                 holidays: [
-                    [1874, 1948, 1, 1, "四方節"],
-                    [1949, 9999, 1, 1, "元日"],
-                    [1874, 1948, 1, 3, "元始祭"],
-                    [1874, 1948, 1, 5, "新年宴会"],
-                    [1949, 1999, 1, 15, "成人の日"],
-                    [2000, 9999, 1, [2, 1], "成人の日"],
-                    [1874, 1912, 1, 30, "孝明天皇祭"],
-                    [1874, 1948, 2, 11, "紀元節"],
-                    [1967, 9999, 2, 11, "建国記念の日"],
-                    [1989, 1989, 2, 24, "昭和天皇の大喪の礼"],
-                    [1879, 1948, 3, "getSyunbun", "春季皇霊祭"],
-                    [1949, 2199, 3, "getSyunbun", "春分の日"],
-                    [1874, 1948, 4, 3, "神武天皇祭"],
-                    [1959, 1959, 4, 10, "皇太子・明仁親王の結婚の儀"],
-                    [1927, 1948, 4, 29, "天長節"],
-                    [1949, 1988, 4, 29, "天皇誕生日"],
-                    [1989, 2006, 4, 29, "みどりの日"],
-                    [2007, 9999, 4, 29, "昭和の日"],
-                    [1949, 9999, 5, 3, "憲法記念日"],
-                    [2007, 9999, 5, 4, "みどりの日"],
-                    [1949, 9999, 5, 5, "こどもの日"],
-                    [1993, 1993, 6, 9, "皇太子・徳仁親王の結婚の儀"],
-                    [1996, 2002, 7, 20, "海の日"],
-                    [2003, 9999, 7, [3, 1], "海の日"],
-                    [1913, 1926, 7, 30, "明治天皇祭"],
-                    [2016, 9999, 8, 11, "山の日"],
-                    [1913, 1926, 8, 31, "天長節"],
-                    [1966, 2002, 9, 15, "敬老の日"],
-                    [2003, 9999, 9, [3, 1], "敬老の日"],
-                    [1874, 1878, 9, 17, "神嘗祭"],
-                    [1878, 1947, 9, "getSyuubun", "秋季皇霊祭"],
-                    [1948, 2199, 9, "getSyuubun", "秋分の日"],
-                    [1966, 1999, 10, 10, "体育の日"],
-                    [2000, 9999, 10, [2, 1], "体育の日"],
-                    [1873, 1879, 10, 17, "神嘗祭"],
-                    [1913, 1926, 10, 31, "天長節祝日"],
-                    [1873, 1911, 11, 3, "天長節"],
-                    [1927, 1947, 11, 3, "明治節"],
-                    [1948, 9999, 11, 3, "文化の日"],
-                    [1990, 1990, 11, 12, "即位の礼正殿の儀"],
-                    [1873, 1947, 11, 23, "新嘗祭"],
-                    [1948, 9999, 11, 23, "勤労感謝の日"],
-                    [1915, 1915, 11, 10, "即位の礼"],
-                    [1915, 1915, 11, 14, "大嘗祭"],
-                    [1915, 1915, 11, 16, "大饗第1日"],
-                    [1928, 1928, 11, 10, "即位の礼"],
-                    [1928, 1928, 11, 14, "大嘗祭"],
-                    [1928, 1928, 11, 16, "大饗第1日"],
-                    [1989, 9999, 12, 23, "天皇誕生日"],
-                    [1927, 1947, 12, 25, "大正天皇祭"]
+                    [1874, 1948, 1, 1, '四方節'],
+                    [1949, 9999, 1, 1, '元日'],
+                    [1874, 1948, 1, 3, '元始祭'],
+                    [1874, 1948, 1, 5, '新年宴会'],
+                    [1949, 1999, 1, 15, '成人の日'],
+                    [2000, 9999, 1, [2, 1], '成人の日'],
+                    [1874, 1912, 1, 30, '孝明天皇祭'],
+                    [1874, 1948, 2, 11, '紀元節'],
+                    [1967, 9999, 2, 11, '建国記念の日'],
+                    [1989, 1989, 2, 24, '昭和天皇の大喪の礼'],
+                    [1879, 1948, 3, 'getSyunbun', '春季皇霊祭'],
+                    [1949, 2199, 3, 'getSyunbun', '春分の日'],
+                    [1874, 1948, 4, 3, '神武天皇祭'],
+                    [1959, 1959, 4, 10, '皇太子・明仁親王の結婚の儀'],
+                    [1927, 1948, 4, 29, '天長節'],
+                    [1949, 1988, 4, 29, '天皇誕生日'],
+                    [1989, 2006, 4, 29, 'みどりの日'],
+                    [2007, 9999, 4, 29, '昭和の日'],
+                    [1949, 9999, 5, 3, '憲法記念日'],
+                    [2007, 9999, 5, 4, 'みどりの日'],
+                    [1949, 9999, 5, 5, 'こどもの日'],
+                    [1993, 1993, 6, 9, '皇太子・徳仁親王の結婚の儀'],
+                    [1996, 2002, 7, 20, '海の日'],
+                    [2003, 9999, 7, [3, 1], '海の日'],
+                    [1913, 1926, 7, 30, '明治天皇祭'],
+                    [2016, 9999, 8, 11, '山の日'],
+                    [1913, 1926, 8, 31, '天長節'],
+                    [1966, 2002, 9, 15, '敬老の日'],
+                    [2003, 9999, 9, [3, 1], '敬老の日'],
+                    [1874, 1878, 9, 17, '神嘗祭'],
+                    [1878, 1947, 9, 'getSyuubun', '秋季皇霊祭'],
+                    [1948, 2199, 9, 'getSyuubun', '秋分の日'],
+                    [1966, 1999, 10, 10, '体育の日'],
+                    [2000, 9999, 10, [2, 1], '体育の日'],
+                    [1873, 1879, 10, 17, '神嘗祭'],
+                    [1913, 1926, 10, 31, '天長節祝日'],
+                    [1873, 1911, 11, 3, '天長節'],
+                    [1927, 1947, 11, 3, '明治節'],
+                    [1948, 9999, 11, 3, '文化の日'],
+                    [1990, 1990, 11, 12, '即位の礼正殿の儀'],
+                    [1873, 1947, 11, 23, '新嘗祭'],
+                    [1948, 9999, 11, 23, '勤労感謝の日'],
+                    [1915, 1915, 11, 10, '即位の礼'],
+                    [1915, 1915, 11, 14, '大嘗祭'],
+                    [1915, 1915, 11, 16, '大饗第1日'],
+                    [1928, 1928, 11, 10, '即位の礼'],
+                    [1928, 1928, 11, 14, '大嘗祭'],
+                    [1928, 1928, 11, 16, '大饗第1日'],
+                    [1989, 9999, 12, 23, '天皇誕生日'],
+                    [1927, 1947, 12, 25, '大正天皇祭']
                 ],
                 /**
                  * 春分の日の日付を取得
