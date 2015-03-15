8a353822a1576dc358d28a4eb6fc44f15701c4f9
hrdaya <info@mar-cielo.com>
2015-03-15 14:51:03 +0900

    v2.2.1

--- dist/UltraDate.js
+++ dist/UltraDate.js
@@ -1,5 +1,5 @@
 /*!
- * UltraDate.js v2.1.1 (http://hrdaya.github.io/UltraDate.js/)
+ * UltraDate.js v2.2.1 (http://hrdaya.github.io/UltraDate.js/)
  *
  * Copyright 2015 yu-ki higa (https://github.com/hrdaya)
  * Licensed under MIT (https://github.com/hrdaya/UltraDate.js/blob/master/LICENSE)
